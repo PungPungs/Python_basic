3d4b65880f0bd35247e463c0733991519b8c7bb5
mini9155 <han_thesis@naver.com>
2025-04-04 15:49:52 +0900

    시스템 완성. 디자인 개선 필요

--- Motor_UI/test/process.py
+++ Motor_UI/test/process.py
@@ -1,12 +1,2 @@
-from multiprocessing import Process, Queue
-import multiprocessing
-
-
-class ProcessHandler:
-    def __init__(self, name, target):
-        self.name = name
-        self.target = target
-        que = Queue()
-        self.que = que
-        p = Process(name=self.name, target=self.target, args=(self.que,), daemon=True)
-    
+with open("./record/sss.txt", 'w') as f:
+    pass
