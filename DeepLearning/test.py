839e32c8fee858d3a87a6c8cdc9a38630587fa47
min <han_thesis@naver.com>
2025-04-09 21:49:52 +0900

    공부한 거 정리 제일 중요한 내용이다.

--- DeepLearning/test.py
+++ DeepLearning/test.py
@@ -1,25 +1,59 @@
-# (delta(y)/delta(x0), delta(y)/delta(x1)) 형태의 백터는 기울기라고 한다.
+import os,sys
+sys.path.append(os.pardir)
 import numpy as np
-def function_2(x):
-    return x[0]**2 + x[1]**2
+
+def softmax(x):
+    return np.exp(x - np.max(x)) / np.sum(np.exp(x - np.max(x)))
+
+def cross_entropy_error(y,t):
+    h = 1e-7
+    return -np.sum(t*np.log(y+h))
 
 def numerical_gradient(f,x):
     h = 1e-4
     grad = np.zeros_like(x)
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp = x[idx]
 
-    for idx in range(x.size):
-        tmp_val = x[idx]
-        x[idx] = tmp_val + h
-        fxh1 = f(x)
+        x[idx] = tmp + h
+        fwh1 = f(x)
 
-        x[idx] = tmp_val - h
-        fxh2 = f(x)
+        x[idx] = tmp - h
+        fwh2 = f(x)
 
-        grad[idx] = (fxh1 - fxh2) / (2*h)
-        x[idx] = tmp_val # 값 원상복구
+        grad[idx] = (fwh1 - fwh2) / (2*h)
+        x[idx] = tmp
+        
     return grad
 
+class SimpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2,3)
 
-print(numerical_gradient(function_2, np.array([3.0,4.0])))
-print(numerical_gradient(function_2, np.array([0.0,2.0])))
-print(numerical_gradient(function_2, np.array([3.0,0.0])))
+    def predict(self, x):
+        return np.dot(x, self.W)
+    
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y,t)
+
+        return loss
+net = SimpleNet()
+
+print(net.W)
+
+x = np.array([0.6,0.9])
+p = net.predict(x)
+
+print(p)
+
+t = np.array([0,0,1])
+net.loss(x,t)
+
+def f(W):
+    return net.loss(x,t)
+
+dw = numerical_gradient(f, net.W)
